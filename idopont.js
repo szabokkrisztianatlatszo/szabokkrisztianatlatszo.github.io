0598523a3275d1edec5a21a2bb8068d4a858af00
szabokkrisztianatlatszo <szabo.krisztian@atlatszo.hu>
2022-03-29 13:11:22 +0200

    idopont.js

---
+++ idopont.js
@@ -0,0 +1,61 @@
+var idopont=0
+var idopontketto=9069266
+
+if (idopont == 1) {
+      $('.nullanulla').css('display','inline-block');
+      $('.hetora').css('display','inline-block');
+      $('.kilencora').css('display','inline-block');
+      $('.kilencora').addClass('selected');
+     $('.ujreszvetel').css('display','inline-block');
+     $('.ujreszvetelatlag').css('display','inline-block');
+} else if (idopont == 2) {
+      $('.nullanulla').css('display','inline-block');
+      $('.hetora').css('display','inline-block');
+      $('.kilencora').css('display','inline-block');
+      $('.tizenegyora').css('display','inline-block');
+      $('.tizenegyora').addClass('selected');
+     $('.ujreszvetel').css('display','inline-block');
+     $('.ujreszvetelatlag').css('display','inline-block');
+} else if (idopont == 3) {
+      $('.nullanulla').css('display','inline-block');
+      $('.hetora').css('display','inline-block');
+      $('.kilencora').css('display','inline-block');
+      $('.tizenegyora').css('display','inline-block');
+      $('.tizenharomora').css('display','inline-block');
+      $('.tizenharomora').addClass('selected');
+     $('.ujreszvetel').css('display','inline-block');
+     $('.ujreszvetelatlag').css('display','inline-block');
+} else if (idopont == 4) {
+      $('.nullanulla').css('display','inline-block');
+      $('.hetora').css('display','inline-block');
+      $('.kilencora').css('display','inline-block');
+      $('.tizenegyora').css('display','inline-block');
+      $('.tizenharomora').css('display','inline-block');
+      $('.tizenotora').css('display','inline-block');
+      $('.tizenotora').addClass('selected');
+     $('.ujreszvetel').css('display','inline-block');
+     $('.ujreszvetelatlag').css('display','inline-block');
+} else if (idopont == 5) {
+      $('.nullanulla').css('display','inline-block');
+      $('.hetora').css('display','inline-block');
+      $('.kilencora').css('display','inline-block');
+      $('.tizenegyora').css('display','inline-block');
+      $('.tizenharomora').css('display','inline-block');
+      $('.tizenotora').css('display','inline-block');
+      $('.tizenhetora').css('display','inline-block');
+      $('.tizenhetora').addClass('selected');
+     $('.ujreszvetel').css('display','inline-block');
+     $('.ujreszvetelatlag').css('display','inline-block');
+} else if (idopont == 6) {
+      $('.nullanulla').css('display','inline-block');
+      $('.hetora').css('display','inline-block');
+      $('.kilencora').css('display','inline-block');
+      $('.tizenegyora').css('display','inline-block');
+      $('.tizenharomora').css('display','inline-block');
+      $('.tizenotora').css('display','inline-block');
+      $('.tizenhetora').css('display','inline-block');
+      $('.tizennyolcharmincora').css('display','inline-block');
+      $('.tizennyolcharmincora').addClass('selected');
+     $('.ujreszvetel').css('display','inline-block');
+     $('.ujreszvetelatlag').css('display','inline-block');
+}
