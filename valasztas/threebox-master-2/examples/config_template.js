0ec6a06e8edb52886a75ddb16caabf7ade953251
szabokkrisztianatlatszo <szabo.krisztian@atlatszo.hu>
2023-01-23 10:12:16 +0100

    up

--- valasztas/threebox-master-2/examples/config_template.js
+++ valasztas/threebox-master-2/examples/config_template.js
@@ -1,4 +1,4 @@
 var config = {
-    accessToken: 'mapbox key goes here',
+    accessToken: 'pk.eyJ1IjoiYXRsbyIsImEiOiJjam5mdDVldmkwbTJ6M3BwNW13ZXhuYXM4In0.gikvkhPkud0GJy1FxG6-Zg',
     subscriptionKey: 'azure maps subscription key goes here'
 };
